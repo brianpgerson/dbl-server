664ef98d13e0e4c45f3df4e73592483ef2b47138
Brian Gerson <brianpgerson@gmail.com>
2025-04-06 19:07:54 -0700

    more

--- db.js
+++ db.js
@@ -2,11 +2,12 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const getDbPool = () => {
+  // Check if we're on Heroku
+  const isHeroku = process.env.DATABASE_URL && process.env.DATABASE_URL.includes('herokuapp.com');
+  
   return new Pool({
     connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/dong_bong_league',
-    ssl: process.env.DATABASE_URL && process.env.DATABASE_URL.includes('herokuapp.com') 
-      ? { rejectUnauthorized: false } 
-      : false
+    ssl: isHeroku ? { rejectUnauthorized: false } : false
   });
 };
 
